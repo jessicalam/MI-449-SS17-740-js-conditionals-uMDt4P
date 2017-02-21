bb2b04c453f88cd164ec9dfe2b00e3c9cd90b4b6
jessicalam <j.lam@mail.com>
2017-02-21 14:29:52 -0500

    fixed conditionals

--- story.js
+++ story.js
@@ -33,17 +33,17 @@ if (boatIntro === 'keep sailing' && r === true) {
 '\nIF YOU WANT TO GO SCUBA DIVING TYPE IN 2' +
 '\nIF YOU WANT TO OBSERVE NATURE TYPE IN 3' +
 '\nIF YOU WANT TO WATCH THE SUNSET TYPE IN 4')
-  if (parseFloat(sailingPara) === '1.0') {
+  if (parseFloat(sailingPara) === 1.0) {
     window.alert('YOU FOUND A BOOT. THAT\'S NOT ENOUGH FOR YOU AND YOUR CREW TO SURVIVE. YOU DIED OF STARVATION. GOOD BYE')
-  } else if (parseFloat(sailingPara) === '2.0' || '3.0') {
+  } else if (parseFloat(sailingPara) === 2.0 || parseFloat(sailingPara) === 3.0) {
     window.alert('YOU TOOK A MINUTE TO LOOK AROUND BEFORE DIVING INTO THE WATER. YOU EVENTUALLY SEE A TREASURE CHEST IN THE WATER. YOU HAVE AMAZING EYES. SO YOU DOVE INTO THE WATER AND HAULED THE TREASURE CHEST UP. YAY YOU STRUCK GOLD. THERE WAS ALSO A MAP IN THE TREASURE CHEST THAT WAS STIL READABLE SO YOU SURVIVED BECAUSE YOU FOUND LAND. YAY.')
-  } else if (parseFloat(sailingPara) === '4.0') {
+  } else if (parseFloat(sailingPara) === 4.0) {
     window.alert('YOU AND YOUR CREW SPENT SOME TIME LOOKNG AT A SUNSET. THAT WAS A DUMB THING TO DO BECAUSE YOU WENT BLIND AND DIED ON THE SHIP BECAUSE NO ONE COULD SEE ANYTHING. SOME PEOPLE SAY YOU ALL DIED BY HITTING A GLACIER, OTHERS SAY THAT YOU ALL DIED BECAUSE YOU WENT FISHING AND COULD ONLY FIND A BOOT, SO YOU DIED OF STARVATION. ONLY YOU KNOW. GOOD BYE')
-  } else if (parseFloat(sailingPara) === null || parseFloat(sailingPara) === undefined) {
-    window.alert('SOMEHOW YOU TYPED SOMETHING WRONG OR THE MATH THINGY DID NOT WORK. REFRESH THE PAGE.')
+  } else if (parseFloat(sailingPara) === null || parseFloat(sailingPara) === undefined || parseFloat(sailingPara) > 4.0) {
+    window.alert('SOMEHOW YOU TYPED SOMETHING WRONG SO THE MATH THINGY DID NOT WORK. REFRESH THE PAGE.')
   } else {
-    window.alert('YOU STOPPED FOR A LITTLE THEN CONTINUED SAILING AND EVENTUALLY ENCOUNTERED LAND. YOU WERE FOUND BY A RESCUE TEAM AND YOU WERE TAKEN HOME. YOU SURVIVED. YAY! CONGRATS')
+    window.alert('SOMEHOW YOU TYPED SOMETHING WRONG SO THE MATH THINGY DID NOT WORK. REFRESH THE PAGE.')
   }
 } else {
-  window.alert('You pressed cancel. Refresh the page.')
+  window.alert('You pressed cancel OR typed something wrong. Refresh the page.')
 }
